b0e13aa29aec266af5b77104a9a8a8636a5842df
Hans Findel <hanstrax@gmail.com>
2014-02-20 21:49:15 -0300

    Start padrino app

---
+++ app/controllers/posts.rb
@@ -0,0 +1,23 @@
+PostsTest::App.controllers :posts do
+  
+  get :index, :map => '/foo/bar' do
+    @posts = Post.all
+    render 'index'
+  end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+
+end
