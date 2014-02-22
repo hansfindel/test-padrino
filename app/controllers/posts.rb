0680d3e9b5e0a5399c3b0bece2eb9106327ddbb0
Hans Findel <hanstrax@gmail.com>
2014-02-21 22:13:05 -0300

    Easy form

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -2,7 +2,14 @@ PostsTest::App.controllers :posts do
   
   get :index, :map => '/foo/bar' do
     @posts = Post.all
+    @post = Post.new
     render 'index'
+  end
+
+  post :create, map: 'post' do 
+    @post = Post.new(params[:post])
+    @post.save 
+    redirect_to "/foo/bar"
   end
 
   # get :sample, :map => '/sample/url', :provides => [:any, :js] do
