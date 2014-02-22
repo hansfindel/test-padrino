722a1287254c67d4b87b03f0d57784e02a728b57
Hans Findel <hanstrax@gmail.com>
2014-02-21 22:28:22 -0300

    Add delete capability

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,6 +1,6 @@
 PostsTest::App.controllers :posts do
   
-  get :index, :map => '/foo/bar' do
+  get :padrino_index, :map => '/' do
     @posts = Post.all
     @post = Post.new
     render 'index'
@@ -9,9 +9,20 @@ PostsTest::App.controllers :posts do
   post :create, map: 'post' do 
     @post = Post.new(params[:post])
     @post.save 
-    redirect_to "/foo/bar"
+    # redirect_to "/"
+    redirect url_for(:posts, :padrino_index)
   end
 
+  delete :delete, map: "post/:id/delete" do 
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+    redirect url_for(:posts, :padrino_index)
+  end
+  get :delete, map: "post/:id/delete" do 
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+    redirect url_for(:posts, :padrino_index)
+  end
   # get :sample, :map => '/sample/url', :provides => [:any, :js] do
   #   case content_type
   #     when :js then ...
